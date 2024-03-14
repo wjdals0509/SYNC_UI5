1f2af3581f16e3dc4a4f3385ef26d22af0205f67
wjdals0509 <wjdals050932@gmail.com>
2024-03-14 19:49:18 +0900

    homework1 생성

---
+++ homework1/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sync/e21/homework1/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ homework1/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"synce21/homework1/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
