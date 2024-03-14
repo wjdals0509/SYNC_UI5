f11575d89e8ccbbc9303d57bb3cef087388f0c4a
wjdals0509 <wjdals050932@gmail.com>
2024-03-14 20:15:47 +0900

    task3까지 수행

--- homework1/webapp/controller/View1.controller.js
+++ homework1/webapp/controller/View1.controller.js
@@ -10,6 +10,13 @@ sap.ui.define([
         return Controller.extend("sync.e21.homework1.controller.View1", {
             onInit: function () {
 
+            },
+            onSelectionChange: function ( oEvent ) {
+                let oItem = oEvent.getParameter("listItem");
+                let oContext = oItem.getBindingContext();
+                let oCarr = oContext.getProperty("Carrid");
+                let oConn = oContext.getProperty("Connid");
+                sap.m.MessageToast.show("선택하신 라인은 항공사: " + oCarr + ", 항공편: " + oConn + "의 정보입니다." );
             }
         });
     });
