29eb8a868ded11bab2e4ee4b72ec0fccc1192e4c
wjdals0509 <wjdals050932@gmail.com>
2024-03-15 15:34:40 +0900

    Main 수정

--- exercise8/webapp/controller/Main.controller.js
+++ exercise8/webapp/controller/Main.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
