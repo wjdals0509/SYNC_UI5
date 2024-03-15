4ed251dd1ebca789e04a0df238587dd9fa5da332
wjdals0509 <wjdals050932@gmail.com>
2024-03-15 15:18:10 +0900

    Main.controller 수정

--- exercise8/webapp/controller/Main.controller.js
+++ exercise8/webapp/controller/Main.controller.js
@@ -23,7 +23,12 @@ sap.ui.define([
                 // Popup.fragment.xml 을 팝업창으로 호출하는 로직
                 let oView = this.getView();
                 let oDialog = this.byId("idDialog");
+                let oInput = this.byId("idInput"); // View 에서 idInput 인 ID 를 가진 Element 를 찾음
+                let name = oInput.getValue(); // 해당 Element 의 getValue 에 메소드를 실행함
+
                 if ( oDialog ){
+                    let oNameText = this.byId("idNameText");
+                    oNameText.setText(name);
                     oDialog.open(); // Fragment를 Load한 적이 있으면 byId로 찾을 수 있따.
                                     // 그렇게 찾은 Fragment를 팝업창으로 보여준다.
                 } else {
@@ -39,6 +44,9 @@ sap.ui.define([
                     // Fragment Load 가 완료되면 Main View에 연결함 ( Main View의 모델도 이용 가능 )
                     oFragment.then(function( oDialog ) {
                         oView.addDependent(oDialog); // View에 연결
+
+                        let oNameText = oView.byId("idNameText");
+                        oNameText.setText(name);
                         oDialog.open(); // 팝업창 출력
                     });
                 }
