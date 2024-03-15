11fb8b96b42cd1779e4e936980087f0ac6280afa
wjdals0509 <wjdals050932@gmail.com>
2024-03-15 12:56:20 +0900

    입력값 점검(messageToast, messageManager)

--- exercise12/webapp/controller/Main.controller.js
+++ exercise12/webapp/controller/Main.controller.js
@@ -1,15 +1,34 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("sync.e21.exercise12.controller.Main", {
             onInit: function () {
+                let data = {
+                    Carrid: "AA",
+                    Connid: "0017",
+                };
+                
+                let oModel = new JSONModel( data );
+                let oView = this.getView();
+                oView.setModel(oModel, "view");
+                
+                // Message Manager를 가져와서 Main View에 등록한다.
+                // 오류가 발생한 위치에 메세지가 붙어있을 수 있도록 이벤트 핸들링 옵션도 true로 설정한다.
+                sap.ui.getCore().getMessageManager().registerObject(oView, true);
+            },
+            onCarridValidError: function ( oEvent ) {
+                let oView = this.getView();
+                let oModel = oView.getModel("view");
+                let data = oModel.getData();
 
+                sap.m.MessageToast.show(data.Carrid + "항공사 ID값이 잘못되었습니다.")
             }
         });
     });
