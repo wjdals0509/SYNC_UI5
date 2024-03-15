791e7476252d97339dc1584035d8e82394a9b0ba
wjdals0509 <wjdals050932@gmail.com>
2024-03-15 13:20:37 +0900

    입력값 점검(정규식 사용)

---
+++ exercise12/webapp/model/type/Connid.js
@@ -0,0 +1,34 @@
+sap.ui.define([
+    "sap/ui/model/SimpleType",
+    "sap/ui/model/ValidateException"
+    ], 
+
+    function( SimpleType, ValidateException ) {
+        'use strict';
+        return SimpleType.extend("sync.e21.exercise12.model.type.Connid", {
+            formatValue: function ( oEvent ) {
+                // 입력된 값 그대로 화면에 출력할 경우
+                return oValue;
+            },
+
+            parseValue: function ( oValue ) {
+                // 화면의 값을 저장할 때 어떻게 변경해서 저장할 것인가
+                return oValue;
+            },
+
+            validValue: function( oValue ) {
+                // /^    : 문자의 시작
+                // \d    : 숫자
+                // \d{4} : 숫자 4개
+                // $/    : 문자의 종료
+                if ( /^\d{4}$/.test( oValue ) ) {
+                    // oValue는 숫자 4개로 구성되어 있다.
+                } else {
+                    // oValue는 숫자 4개로 구성되어 있지 않다.
+                    // 오류
+                    // ABAP에서는 raise exception과 동일
+                    throw new ValidateException( "항공편은 숫자 4자리로 입력해주세요." );
+                }
+            }
+        });
+    });
