b6411c54e4829950e222ef5f5a339a8526bc3803
wjdals0509 <wjdals050932@gmail.com>
2024-03-15 12:23:06 +0900

    view1 주석제거

--- homework1/webapp/controller/View1.controller.js
+++ homework1/webapp/controller/View1.controller.js
@@ -18,17 +18,14 @@ sap.ui.define([
                 let oConn = oContext.getProperty("Connid");
                 sap.m.MessageToast.show("선택하신 라인은 항공사: " + oCarr + ", 항공편: " + oConn + "의 정보입니다." );
 
-                // Popup.fragment.xml 을 팝업창으로 호출하는 로직
                 let oView = this.getView();
                 let oDialog = this.byId("idDialog");
                 if ( oDialog ){
                     oCarr.setText(Carrid);
                     
-                    oDialog.open(); // Fragment를 Load한 적이 있으면 byId로 찾을 수 있따.
-                                    // 그렇게 찾은 Fragment를 팝업창으로 보여준다.
+                    oDialog.open(); 
+                                    
                 } else {
-                    // Popup.fragment.xml 파일을 읽어옴
-                    // Controller도 연결함
                     let oFragment = sap.ui.core.Fragment.load({
                         id: oView.getId(),
                         name: "sync.e21.homework1.view.Info",
@@ -36,10 +33,9 @@ sap.ui.define([
                         controller: this
                     });
 
-                    // Fragment Load 가 완료되면 Main View에 연결함 ( Main View의 모델도 이용 가능 )
                     oFragment.then(function( oDialog ) {
-                        oView.addDependent(oDialog); // View에 연결
-                        oDialog.open(); // 팝업창 출력
+                        oView.addDependent(oDialog); 
+                        oDialog.open(); 
                     });
                 }
                 
