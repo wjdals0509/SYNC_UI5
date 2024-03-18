2c61fa2b7941e79887db55cfc13a915e2ea3b6c6
wjdals0509 <wjdals050932@gmail.com>
2024-03-18 12:04:44 +0900

    선택한 라인의 정보가 담긴 MessageToast 띄우기

--- hw1review/webapp/controller/Main.controller.js
+++ hw1review/webapp/controller/Main.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
 
         return Controller.extend("sync.e21.hw1review.controller.Main", {
@@ -13,7 +14,12 @@ sap.ui.define([
             },
             // oEvent는 선택한 라인의 데이터를 담고 있음
             onSelectionChange: function ( oEvent ) {
-                debugger;
+                let olistItem = oEvent.getParameter("listItem");
+                let oContext = olistItem.getBindingContext();
+                let carrid = oContext.getProperty("Carrid");
+                let connid = oContext.getProperty("Connid");
+
+                MessageToast.show("선택하신 라인은 항공사:" + carrid + ", 항공편:" + connid + "의 정보입니다.");
             } 
         });
     });
