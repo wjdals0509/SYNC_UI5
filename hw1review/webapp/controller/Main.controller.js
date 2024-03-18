1a077d44c1e3b0564c4575a4b735ca7bfd10edc6
wjdals0509 <wjdals050932@gmail.com>
2024-03-18 12:00:25 +0900

    oEvent에 대한 정보 확인(디버깅)

--- hw1review/webapp/controller/Main.controller.js
+++ hw1review/webapp/controller/Main.controller.js
@@ -10,6 +10,10 @@ sap.ui.define([
         return Controller.extend("sync.e21.hw1review.controller.Main", {
             onInit: function () {
 
-            }
+            },
+            // oEvent는 선택한 라인의 데이터를 담고 있음
+            onSelectionChange: function ( oEvent ) {
+                debugger;
+            } 
         });
     });
