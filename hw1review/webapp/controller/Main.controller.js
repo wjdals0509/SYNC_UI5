df09ed972a93c3e503605a16874d6a1ee5abe486
wjdals0509 <wjdals050932@gmail.com>
2024-03-18 12:50:58 +0900

    팝업창 내용에 데이터 넣기

--- hw1review/webapp/controller/Main.controller.js
+++ hw1review/webapp/controller/Main.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Fragment) {
         "use strict";
 
         return Controller.extend("sync.e21.hw1review.controller.Main", {
@@ -20,6 +21,38 @@ sap.ui.define([
                 let connid = oContext.getProperty("Connid");
 
                 MessageToast.show("선택하신 라인은 항공사:" + carrid + ", 항공편:" + connid + "의 정보입니다.");
+
+                // Fragment의 Dialog를 오픈하도록 한다.
+                let oView = this.getView();
+                let oDialog = oView.byId("idDialog");
+                // 아래 2가지 경우도 가능하긴 하지만 위의 구문이 정석
+                // oDialog = this.byId("idDialog");
+                // oDialog = this.getView().byId("idDialog");
+
+                // oModel 경로를 현재경로로 취급
+                let currnetModelPath = oContext.getPath();
+                oView.bindElement(currnetModelPath);
+
+                if (oDialog) {
+                    // Main 화면에서 있을 때
+                    oDialog.open();
+                } else {
+                    // Main 화면에서 없을 때
+                    Fragment.load({
+                        id: oView.getId(),
+                        name: "sync.e21.hw1review.view.Info",
+                        type: "XML",
+                        controller: this
+                    }).then(   
+                        // load가 완료됐을 때
+                        function ( oDialog ) {
+                            oView.addDependent(oDialog);
+                            oDialog.open();
+                        }
+                    ); 
+
+                }
+
             } 
         });
     });
