4b9d6dcc1777a76d36321e5975d42e41ea3ecafe
wjdals0509 <wjdals050932@gmail.com>
2024-03-19 16:33:46 +0900

    생성버튼 로직 구현 및 취소버튼 로직 구현

--- odatamodel/webapp/controller/Main.controller.js
+++ odatamodel/webapp/controller/Main.controller.js
@@ -1,15 +1,59 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Fragment, JSONModel) {
         "use strict";
 
         return Controller.extend("sync.e21.odatamodel.controller.Main", {
+            initAirlineData: {
+                Carrid: "",
+                Carrname: "",
+                Currcode: "",
+                Url: ""
+            },
+            
             onInit: function () {
+                let data = this.initAirlineData;
+                let oNewModel = new JSONModel(data);
+                let oView = this.getView();
+                oView.setModel(oNewModel, "new");
+            },
 
+            onCreate: function () {
+                let oView = this.getView();
+                let oDialog = oView.byId("idNewDialog");
+
+                if ( oDialog ) {
+                    oDialog.open();
+                } else {
+                    Fragment.load({
+                        id: oView.getId(),
+                        name: "sync.e21.odatamodel.view.New",
+                        type: "XML",
+                        controller: this             // View의 Controller를 공유
+                    }).then(function(oDialog){       
+                        oView.addDependent(oDialog); // View의 Model을 공유
+                        oDialog.open();
+                    });
+                }
+            },
+
+            onSaveCancel: function () {
+                let oDialog = this.getView().byId("idNewDialog");
+                if( oDialog ) {
+                    oDialog.close();
+                }
+
+                // 빈값만 있는 정보고 새로운 JSONModel을 만들어서
+                // 기존의 new 모델을 교체해버림 => 데이터 초기화
+                let oNewModel = new JSONModel(this.initAirlineData);
+                this.getView().setModel(oNewModel, "new");
             }
+            
         });
     });
