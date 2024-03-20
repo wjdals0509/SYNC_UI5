696de7a4fd51a8dd83aa4efdff17a673aaa327ca
wjdals0509 <wjdals050932@gmail.com>
2024-03-20 17:53:49 +0900

    통화코드에 데이터 넣기

--- odatamodel_multi_delete/webapp/controller/Main.controller.js
+++ odatamodel_multi_delete/webapp/controller/Main.controller.js
@@ -1,15 +1,27 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("sync.e21.odatamodelmultidelete.controller.Main", {
             onInit: function () {
+                let data = {
+                    Currency:[
+                        {Key:"KRW", Name:"원화"},
+                        {Key:"USD", Name:"달러"},
+                        {Key:"EUR", Name:"유로"},
+                        {Key:"AUD", Name:"호주"},
+                        {Key:"CAD", Name:"캐나다"},
+                    ]
+                };
 
+                let oViewModel = new JSONModel(data);
+                this.getView().setModel(oViewModel, "view");
             }
         });
     });
