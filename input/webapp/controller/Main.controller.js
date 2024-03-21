08c2b64ca4e081a5e6ce2d2de0e8eef7c86bb497
wjdals0509 <wjdals050932@gmail.com>
2024-03-21 12:05:09 +0900

    정수끼리 더하기 연산 구현

--- input/webapp/controller/Main.controller.js
+++ input/webapp/controller/Main.controller.js
@@ -13,6 +13,21 @@ sap.ui.define([
             },
             onAdd: function () {
                 sap.m.MessageToast.show("더하기 버튼을 눌렀습니다,");
+
+                let oView = this.getView();
+                let oInput1 = oView.byId("idInput1"); 
+                let oInput2 = oView.byId("idInput2");
+                let oText = oView.byId("idText");
+                
+                let value1 = oInput1.getValue();
+                let value2 = oInput2.getValue();
+
+                // 정수 value1, value2 계산 결과
+                // 가져온 값을 정수로 취급하면, 소수가 없어짐
+                let result = parseInt(value1) + parseInt(value2);
+
+                // 계산 결과를 기록
+                oText.setText("계산결과는?? ==>" + result);
             }
         });
     });
