def60ec64cbf12fce543fc7420e40b5e8598a33f
wjdals0509 <wjdals050932@gmail.com>
2024-03-21 11:57:59 +0900

    기본 레이아웃 셋팅 및 onAdd 함수 확인

--- input/webapp/controller/Main.controller.js
+++ input/webapp/controller/Main.controller.js
@@ -10,6 +10,9 @@ sap.ui.define([
         return Controller.extend("sync.e21.input.controller.Main", {
             onInit: function () {
 
+            },
+            onAdd: function () {
+                sap.m.MessageToast.show("더하기 버튼을 눌렀습니다,");
             }
         });
     });
