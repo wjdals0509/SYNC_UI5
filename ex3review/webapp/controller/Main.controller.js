505c759c804ec339423d83461f0e3eb5722434ef
wjdals0509 <wjdals050932@gmail.com>
2024-03-25 17:45:57 +0900

    팝업창 띄우기

--- ex3review/webapp/controller/Main.controller.js
+++ ex3review/webapp/controller/Main.controller.js
@@ -10,6 +10,31 @@ sap.ui.define([
         return Controller.extend("sync.e21.ex3review.controller.Main", {
             onInit: function () {
 
+            },
+
+            onCreate: function () {
+                //sap.m.MessageToast.show("신규생성 버튼을 눌렀습니다.");
+
+                let oView = this.getView();
+                let oDialog = oView.byId("idDialog");
+
+                if ( oDialog ) {
+                    oDialog.open();
+                } else {
+                    sap.ui.core.Fragment.load({
+                        id: oView.getId(),
+                        name: 'sync.e21.ex3review.view.New',
+                        type: 'XML',
+                        controller: this
+                    }).then(function(oDialog) {
+                        oView.addDependent(oDialog);
+                        oDialog.open();
+                    });
+                }
+            },
+
+            onClose: function () {
+                this.byId("idDialog").close();
             }
         });
     });
