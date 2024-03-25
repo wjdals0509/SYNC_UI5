1ab32570f1d4c09fa35d06fca5ec9f1812d5b075
wjdals0509 <wjdals050932@gmail.com>
2024-03-25 11:08:58 +0900

    실습평가 1번 리뷰

---
+++ ux1review/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sync/e21/ux1review/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ ux1review/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"synce21/ux1review/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
