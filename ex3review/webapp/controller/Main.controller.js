13cb54e0cc676606694a4bc8ee348e777003780a
wjdals0509 <wjdals050932@gmail.com>
2024-03-26 10:22:50 +0900

    신규생성 로직 구현

--- ex3review/webapp/controller/Main.controller.js
+++ ex3review/webapp/controller/Main.controller.js
@@ -10,23 +10,25 @@ sap.ui.define([
         return Controller.extend("sync.e21.ex3review.controller.Main", {
             onInit: function () {
                 let data = {
-                    data: {
+                    Data: {
                         Carrid: "",
                         Connid: "0000",
-                        Countryfr: "",
+                        Coutnryfr: "",
                         Cityfrom: "",
+                        Airpfrom: "",
                         Countryto: "",
                         Cityto: "",
                         Airpto: "",
                         Distance: 0,
-                        Distid: "KM" // 기본값
+                        Distid: "KM"    // 기본값
                     },
                     DistanceUnit: [
-                        {key: "KM", name: "킬로미터"},
-                        {key: "MI", name: "마일"}
+                        { key: "KM", name: "킬로미터" },
+                        { key: "MI", name: "마일" }
                     ]
                 }
-                let oModel = new sap.ui.model.json.JSONModel( data );
+
+                let oModel = new sap.ui.model.json.JSONModel( data ) ;
                 this.getView().setModel(oModel, "new");
             },
 
@@ -53,6 +55,28 @@ sap.ui.define([
 
             onClose: function () {
                 this.byId("idDialog").close();
+            },
+
+            
+            onSave: function () {
+                let oView = this.getView();
+                let oModel = oView.getModel(); // SAP Gateway에 연결된 Model
+                let oNewModel = oView.getModel("new"); // JSON Model 팝업의 Input 데이터를 담당
+                let newData = oNewModel.getProperty("/Data");
+
+                // create( 경로, 신규데이터, 결과처리)
+                oModel.create(
+                    "/ConnectionSet",
+                    newData,
+                    { 
+                        success: function ( oData, oResponse ) {
+                            sap.m.MessageBox.success( oData.Carrid + "," + oData.Connid + "가 생성되었습니다.");
+                        }, 
+                        error: function ( oError ) {
+                            console.log(oError);
+                            sap.m.MessageBox.error("생성 중 오류가 발생했습니다.");
+                    }}
+                );
             }
         });
     });
