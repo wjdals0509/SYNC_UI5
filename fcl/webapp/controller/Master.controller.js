2d81e9d66d7ce1bb34d11c43a582fe2a7db064dd
wjdals0509 <wjdals050932@gmail.com>
2024-05-08 12:42:31 +0900

    detail 화면 띄우기 전까지

--- fcl/webapp/controller/Master.controller.js
+++ fcl/webapp/controller/Master.controller.js
@@ -1,15 +1,52 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	'sap/ui/model/Sorter',
+	'sap/m/MessageBox',
+	'sap/f/library'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator, Sorter, MessageBox, fioriLibrary) {
         "use strict";
 
         return Controller.extend("sync.eb.fcl.controller.Master", {
             onInit: function () {
+                this.oView = this.getView();
+                this._bDescendingSort = false;
+                this.oCarrierTable = this.oView.byId("idCarrierTable");
+            },
 
+            onSearch: function ( oEvent ) {
+                var aFilter = [],
+                    sQuery = oEvent.getParameter("query");
+    
+                if (sQuery && sQuery.length > 0) {
+                    // 항공사명으로 검색 가능하게
+                    var oFilter = new Filter("Carrname", FilterOperator.Contains, sQuery);
+                    aFilter.push(oFilter);
+                }
+    
+                this.oCarrierTable.getBinding("items").filter(aFilter, "Application");
+            },
+
+            onSort: function ( oEvent ) {
+                // sort 정보를 역으로바꾸기 위해 !를 사용
+                // 내림차순 -> 오름차순으로 바꾸고
+                // 오름차순 -> 내림차순으로 바꾸기 위해 true <=> false로 전환
+                this._bDescendingSort = !this._bDescendingSort;
+                var oBinding = this.oCarrierTable.getBinding("items"),
+                    oSorter = new Sorter("Carrname", this._bDescendingSort);
+    
+                oBinding.sort(oSorter);
+            },
+            
+            onListItemPress: function () {
+                var oFCL = this.oView.getParent().getParent();
+    
+                oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
             }
         });
     });
