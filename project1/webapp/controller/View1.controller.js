4db3dad076ef4c268e07ef5b68b720bb4fa358c9
wjdals0509 <wjdals050932@gmail.com>
2024-05-17 15:53:20 +0900

    손익계산서 템플릿(full screen)

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -64,69 +64,6 @@ sap.ui.define([
                         "Unit Price": 1456.91,
                         "Units Available": 17996,
                         " Units Sold": 45346
-                    }, {
-                        "City": "New York ",
-                        "Cost": 243875.22,
-                        "Item Category": "Drama",
-                        "Profit": 124653.66,
-                        "Revenue": 368528.87,
-                        "Unit Price": 1369.83,
-                        "Units Available": 23662,
-                        "Units Sold": 66737
-                    }, {
-                        "City": "New York ",
-                        "Cost": 294962,
-                        "Item Category": "Horror Movies",
-                        "Profit": 124668.09,
-                        "Revenue": 419630.09,
-                        "Unit Price": 1658.12,
-                        "Units Available": 26538,
-                        "Units Sold": 78528
-                    }, {
-                        "City": "New York ",
-                        "Cost": 20362.45,
-                        "Item Category": "Pop",
-                        "Profit": 7955.24,
-                        "Revenue": 28317.7,
-                        "Unit Price": 148.76,
-                        "Units Available": 6227,
-                        "Units Sold": 16389
-                    }, {
-                        "City": "New York ",
-                        "Cost": 253598.76,
-                        "Item Category": "Rock",
-                        "Profit": 141583.73,
-                        "Revenue": 395182.49,
-                        "Unit Price": 1337.09,
-                        "Units Available": 14801,
-                        "Units Sold": 579010
-                    }, {
-                        "City": "New York ",
-                        "Cost": 142550.68,
-                        "Item Category": "Soul / R&B",
-                        "Profit": 80381.86,
-                        "Revenue": 222932.54,
-                        "Unit Price": 1250.65,
-                        "Units Available": 11900,
-                        "Units Sold": 321511
-                    }, {
-                        "City": "New York ",
-                        "Cost": 174617.63,
-                        "Item Category": "TV's",
-                        "Profit": 87620.46,
-                        "Revenue": 262238.09,
-                        "Unit Price": 967.09,
-                        "Units Available": 16738,
-                        "Units Sold": 646512
-                    }, {
-                        "City": "New York ",
-                        "Cost": 42853.89,
-                        "Item Category": "Video Equipment",
-                        "Profit": 23242.33,
-                        "Revenue": 66096.22,
-                        "Unit Price": 209.79,
-                        "Units Available": 6344,
-                        "Units Sold": 2007
                     }]
                 });
                 this.getView().setModel(oModel);
