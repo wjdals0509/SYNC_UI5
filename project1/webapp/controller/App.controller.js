bd7413d87f51c95bd1ecf62cf9d7d8b3ac296ba2
wjdals0509 <wjdals050932@gmail.com>
2024-05-17 15:16:56 +0900

    손익계산서 템플릿

---
+++ project1/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("sync.zeb.project1.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ project1/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"synczeb/project1/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
