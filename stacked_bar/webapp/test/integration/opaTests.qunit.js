4336c9a5d7c5304e3f7338f29b8917e2c0ca7b0f
wjdals0509 <wjdals050932@gmail.com>
2024-05-23 09:50:24 +0900

    감가상각 차트

---
+++ stacked_bar/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sync/zeb/stackedbar/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ stacked_bar/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"synczeb/stacked_bar/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
