24be5e9e8f9e2665f1e08dff7e521192a0427b31
wjdals0509 <wjdals050932@gmail.com>
2024-05-26 16:35:12 +0900

    percentage chart

--- stacked_bar/webapp/controller/View1.controller.js
+++ stacked_bar/webapp/controller/View1.controller.js
@@ -1,14 +1,18 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
 
-        return Controller.extend("sync.zeb.stackedbar.controller.View1", {
-            onInit: function () {
-            }
-        });
+    return Controller.extend("sync.zeb.stackedbar.controller.View1", {
+      onInit: function () {
+        // var oVizFrame = this.byId("idVizFrame");
+        // // Attach popover
+        // var oPopover = new Popover({});
+        // oPopover.connect(oVizFrame.getVizUid());
+      },
     });
+  }
+);
