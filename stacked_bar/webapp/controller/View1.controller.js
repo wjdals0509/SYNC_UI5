4382fde89b1a7b0ff623ae225a1d2c87c88416c0
wjdals0509 <wjdals050932@gmail.com>
2024-05-26 23:58:40 +0900

    팝업창 수정전

--- stacked_bar/webapp/controller/View1.controller.js
+++ stacked_bar/webapp/controller/View1.controller.js
@@ -1,17 +1,26 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller"],
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/viz/ui5/controls/Popover",
+    "sap/ui/core/format/NumberFormat",
+  ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller) {
+  function (Controller, Popover, NumberFormat) {
     "use strict";
 
     return Controller.extend("sync.zeb.stackedbar.controller.View1", {
       onInit: function () {
-        // var oVizFrame = this.byId("idVizFrame");
-        // // Attach popover
-        // var oPopover = new Popover({});
-        // oPopover.connect(oVizFrame.getVizUid());
+        var oVizFrame = this.byId("idVizFrame");
+        // Attach popover
+        var oPopover = new Popover({});
+        var oFormat = NumberFormat.getFloatInstance({
+          minFractionDigits: 0,
+          maxFractionDigits: 2,
+        });
+        oPopover.connect(oVizFrame.getVizUid());
+        oPopover.setFormatString(oFormat);
       },
     });
   }
